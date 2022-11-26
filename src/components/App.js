45eda641c4d4f4d206ef3443d303a981d6a161bf
Pintu456 <112724876+Pintu456@users.noreply.github.com>
2022-11-26 13:04:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import '../styles/App.css';
 class Odd extends React.Component {
 
   render() {
+    console.log("Even is unmounted");
     return (
       <div id="odd">
         I am odd
@@ -15,6 +16,7 @@ class Odd extends React.Component {
 class Even extends React.Component {
 
   render() {
+    console.log("Odd is unmounted");
     return (
       <div id="even">
         I am even
@@ -23,13 +25,13 @@ class Even extends React.Component {
   }
 }
 class App extends React.Component {
-  handleChange(){
-    this.setState({even: !this.state.even})
+  handleChange() {
+    this.setState({ even: !this.state.even })
   }
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state = {even: true}
-    this.handleChange =  this.handleChange.bind(this)
+    this.state = { even: true }
+    this.handleChange = this.handleChange.bind(this)
   }
   render() {
     return (
